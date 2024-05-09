cdb8fdf88e77ace8382fab22190f7a9d8f8dbce9
rihabcherni <rihabcherni000@gmail.com>
2024-05-09 13:49:46 +0100

    09/05

--- quiz/models.py
+++ quiz/models.py
@@ -10,7 +10,7 @@ class Quiz(models.Model):
     description_image = models.CharField(max_length=255)
     created_at  = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    
     def __str__(self):
         return f"{self.titre}"
     
--- quiz/serializers.py
+++ quiz/serializers.py
@@ -1,22 +1,35 @@
 from rest_framework import serializers
 from .models import Quiz, QuestionQuiz, ReponseQuestionQuiz, ApprenantQuizReponse
 
-class QuizSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Quiz
-        fields = '__all__'
-
-class QuestionQuizSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = QuestionQuiz
-        fields = '__all__'
-
 class ReponseQuestionQuizSerializer(serializers.ModelSerializer):
     class Meta:
         model = ReponseQuestionQuiz
         fields = '__all__'
 
+class QuestionQuizSerializer(serializers.ModelSerializer):
+    reponses = serializers.SerializerMethodField(method_name='get_reponses', read_only=True)
+    class Meta:
+        model = QuestionQuiz
+        fields = '__all__'
+
+    def get_reponses(self, obj):
+        reponses = ReponseQuestionQuiz.objects.filter(question=obj)
+        serializer = ReponseQuestionQuizSerializer(reponses, many=True)
+        return serializer.data
+    
 class ApprenantQuizReponseSerializer(serializers.ModelSerializer):
     class Meta:
         model = ApprenantQuizReponse
         fields = '__all__'
+
+class QuizSerializer(serializers.ModelSerializer):
+    questions = serializers.SerializerMethodField(method_name='get_questions', read_only=True)
+    
+    class Meta:
+        model = Quiz
+        fields = '__all__'
+
+    def get_questions(self, obj):
+        questions = QuestionQuiz.objects.filter(quiz=obj)
+        serializer = QuestionQuizSerializer(questions, many=True)
+        return serializer.data
